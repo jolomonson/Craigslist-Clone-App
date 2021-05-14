15de57cdb0289b213f76a5717a6ddad9ff5231ee
John Solomon <jolomonson@gmail.com>
2021-05-14 20:33:49 +0100

    working on web scrapping

--- craigslistCloneApp/views.py
+++ craigslistCloneApp/views.py
@@ -2,21 +2,21 @@ import requests
 from django.shortcuts import render
 from requests.compat import quote_plus
 from bs4 import BeautifulSoup
-
-Base_Craigslist_URL = 'https://'
+from . import models
+BaseCraigslistURL = 'https://www.searchcraigslist.org/'
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def newSearch(request):
     search = request.POST.get('search')
-    '''
-    response = requests.get('https://.../{search|title}')
+    models.Search.objects.create(search=search)
+    finalURL = BaseCraigslistURL.format(quote_plus(search))
+    response = requests.get(finalURL)
     data = response.text
     print(data)
-    '''
     stuffForFrontEnd = {
         'search':search,
-
     }
+   
     return render(request, 'myApp/newSearch.html', stuffForFrontEnd)
