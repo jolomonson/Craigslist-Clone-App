3083a05e96139e7352f6bd07810716e1d8c15783
Mary Solomon <jolomonson@gmail.com>
2022-04-28 09:54:55 +0100

    Registered jazzmin app in settings.py
    Signed-off-by: Mary Solomon <jolomonson@gmail.com>

--- craigslistClone/settings.py
+++ craigslistClone/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Pre-loading Third Party apps
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- craigslistCloneApp/models.py
+++ craigslistCloneApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 # Create your models here.
 class Search(models.Model):
     search = models.CharField(max_length=500)
-    created = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return '{}'.format(self.search)
