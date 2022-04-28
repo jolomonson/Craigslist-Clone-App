d53e80929f9239b12aa9c898065bc81e38b739b0
Mary Solomon <jolomonson@gmail.com>
2022-04-28 10:02:27 +0100

    Included list display and filter for admin model
    Signed-off-by: Mary Solomon <jolomonson@gmail.com>

--- craigslistCloneApp/admin.py
+++ craigslistCloneApp/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import Search
 # Register your models here.
+
+class SearchAdmin(admin.ModelAdmin):
+    list_display = (
+        'search',
+        'min_price',
+        'max_price'
+    )
+    list_filter = (
+        'search',
+        'min_price',
+        'max_price'
+    )
 admin.site.register(Search)
--- craigslistCloneApp/models.py
+++ craigslistCloneApp/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 class Search(models.Model):
     search = models.CharField(max_length=500)
+    min_price = models.CharField(max_length=100)
+    max_price = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
