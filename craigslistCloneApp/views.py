641e9427efdbbaa7262890ebc3118e126c4e864d
Mary Solomon <jolomonson@gmail.com>
2022-05-02 10:49:04 +0100

    Scraaped images in different cards
    Signed-off-by: Mary Solomon <jolomonson@gmail.com>

--- craigslistCloneApp/views.py
+++ craigslistCloneApp/views.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 from . import models
 #Note: Use local city's address
 BASE_CRAIGSLIST_URL = "https://losangeles.craigslist.org/search/sss?query={}"
+BASE_IMAGE_URL = "https://images.craigslist.org/{}_300x300.jpg"
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
@@ -31,7 +32,13 @@ def new_search(request):
             post_price = post.find(class_='result-price').text
         else:
             post_price = "N/A"
-        final_postings.append((post_title, post_url, post_price))
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+        else:
+            post_image_url = "https://images.craigslist.org/images/peace.jpg"
+        final_postings.append((post_title, post_url, post_price, post_image_url))
+        
     stuffForFrontEnd = {
         'search':search,
         'final_postings':final_postings
