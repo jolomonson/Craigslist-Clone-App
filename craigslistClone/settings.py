645cc4012379f14d23c31fd664ffeaf455c4c1a1
Mary Solomon <jolomonson@gmail.com>
2022-05-02 15:15:36 +0100

    Saved Changes

--- craigslistClone/settings.py
+++ craigslistClone/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 
@@ -124,3 +124,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+django_heroku.settings(locals())
